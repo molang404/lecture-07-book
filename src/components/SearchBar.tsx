c0961b6b8b0ab573fb208d776d28b919dcde9410
molang404 <art104233@naver.com>
2026-04-20 15:26:43 +0900

    search 완성

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -12,19 +12,20 @@ function SearchBar() {
         event.preventDefault();
 
         // 사용자가 그냥 엔터 쳐버리는 경우
-        if (keyword === "") return;
+        // trim을 했더니, 값이 빈 스트링이면 return으로 끝내라
+        const k = keyword.trim();
+        if (!k) return;
 
         // 생각보다, 이 input에 스페이스바를 넣는 사람이 엄청 많음 "수학" => " 수학"
         // string의 맨 앞과 맨 뒤에 존재할 수 있는 공백을 제거하는 메서드 : trim()
         // "" 에서 사용자가 enter -> keyword.trim() -> ""   -> if (keyword.trim()) -> false
         // " " 에서 사용자가 enter -> keyword.trim() -> ""
-        if (!keyword.trim()) return; // trim을 했더니, 값이 빈 스트링이면 return으로 끝내라
 
         // keyword를 활용해서 사용자를 강제 이동
         // search 라고 하는 경로에, query string으로 keyword를 전달해서 사용자 이동
         // 원래 URL은 한글이 X, 영어랑 몇 가지의 특수문자만 가능
         // 해석을 할 수 있도록 영어랑 몇 가지의 특수문자를 이용해 변환 작업이 필요함 -> encodeURIComponent()
-        navigate(`/search?keywords=${encodeURIComponent(keyword)}`);
+        navigate(`/search?keyword=${encodeURIComponent(keyword)}`);
     };
 
     const onChange = (event: ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -1,41 +1,88 @@
-import { useSearchParams } from "react-router";
+import { Link, useSearchParams } from "react-router";
 import { useEffect, useState } from "react";
+import styles from "./Search.module.css";
 
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
-type BookItem = {
+export type BookItem = {
     id: string;
     volumeInfo: {
         title: string;
-        author?: string[];
-        description: string;
-        imageLink?: {
+        authors?: string[];
+        description?: string;
+        publishDate: string;
+        imageLinks?: {
             thumbnail?: string;
             small?: string;
-        }
-    }
-}
+        };
+    };
+};
 
+type ApiResponseType = { items: BookItem[] };
 
+// JSON에는 null이 없음 / undefined 없음
+// key=value 되어져 있는 건, 값이 "있을 때만"
+// API 명세롤 보고 타입 작성
+
+// 사용자가 요청한 keyword를 받아서, 그것을 가지고 google API 요청을 하고, 받아온 결과를 화면에 출력해주는 일
 function Search() {
     // query string으로 들어온 값을 꺼내오기 위해서는 useSearchParams를 사용
 
     // useParams를 사용할 때는 const { id } = useParams();
     // useSearchParms는 useParams와 사용법이 다름
-    const [params, setParams] = useSearchParams();    // queryString 내용이 params에 담겨 나옴
-    const k = params.get("keyword");   // "수학"이라는 게 있을 수도 있지만, 없을 수도 있음
+    // keyword를 쿼리스트링으로 받겠다.
+    const [params] = useSearchParams(); // queryString 내용이 params에 담겨 나옴
+    // 이렇게 받아온 params라고 하는 state의 값은 객체
+    const k = params.get("keyword"); // *URL*에서 가져온 값 -> 있을 수도 있지만, 없을 수도 있음
 
     // keyword 준비 됐으나, API를 통해 요청한 내용을 받아다가 화면에 출력만 해주면 됨
-    const [loading, setLoading] = useState(true);
-    const [list, setList] = useState([]);
+    const [list, setList] = useState<BookItem[]>([]);
 
     useEffect(() => {
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=${p}&maxResults=20&key=${API_KEY}`);
+        if (!k) return;
 
-    }, []);
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${k}&maxResults=20&key=${API_KEY}`)
+            .then(response => response.json())
+            .then((json: ApiResponseType) => {
+                // list라고 하는 state에 가공한 데이터 저장
+                setList(json.items);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [k]);
 
     return (
-        <div></div>
+        <div className={styles.wrap}>
+            <h3>검색 결과 : {k}</h3>
+
+            {/* 검색 결과 (책 목록) 출력 */}
+            {/* 데이터가 도착했는지 안 했는지, 목록이 있는지 없는지 판단 해줘야 되나? */}
+            {/*{list.length === 0 && <div>검색 결과가 없습니다.</div>}*/}
+            {list.map((value, index) => (
+                <Link key={index} to={`/detail/${value.id}`} className={styles.item}>
+                    {value.volumeInfo.imageLinks ? (
+                        <img
+                            src={value.volumeInfo.imageLinks?.thumbnail}
+                            alt={value.volumeInfo.title}
+                            className={styles.cover}
+                        />
+                    ) : (
+                        <div className={styles.noCover}>No Cover</div>
+                    )}
+                    <div>
+                        <div className={styles.title}>{value.volumeInfo.title}</div>
+                        {/*
+                            array에서 사용할 수 있는 메소드 join()
+                            각 요소를 순회해서 하나의 값을 리턴하는데
+                            각 요소 사이애 [매개변수로 제공된 스트링]을 넣어준다.
+                        */}
+                        <div className={styles.authors}>{value.volumeInfo.authors?.join(", ")}</div>
+                    </div>
+                </Link>
+            ))}
+            {/* map이라고 하는 메소드는 꼭 대상이 array여야만 쓸 수 있음 */}
+        </div>
     );
 }
 
